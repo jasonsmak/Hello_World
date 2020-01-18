29f9e40cf47d8e449e48f30dd7293d585bf4feda
Jason <60024840+jasonsmak@users.noreply.github.com>
2020-01-18 15:20:11 -0500

    Create hello_world.c
    learn to branch, commit and merge with main offline

---
+++ hello_world.c
@@ -0,0 +1,12 @@
+// my hello world program to experiment with github offline
+// to learn how to branch, commit, and merge.
+
+#include <stdio.h>
+
+int main()
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+
+   return 0;
+}
